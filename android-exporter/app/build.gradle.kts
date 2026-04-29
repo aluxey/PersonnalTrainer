7d49f424ad172cb5bcc00305d5ba94b5620ffbbf
aluxey <aurelien.luxeyy@gmail.com>
2026-04-29 22:15:54 +0200

    fix action build apk

--- android-exporter/app/build.gradle.kts
+++ android-exporter/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.personaltrainer.exporter"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.personaltrainer.exporter"
